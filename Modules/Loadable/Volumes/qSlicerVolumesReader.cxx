9b53aef360063d005bf20b41733e0290aa0eb7df
Julien Finet <julien.finet@kitware.com>
2014-09-16 10:00:12 -0400

    ENH: [AtlasViewer] Add vtkMRMLApplicationLogic::FitSlicesToVolume
    Volume reader has an option ("fitSlicesToVolumes") to control the
    fitting of the slices at loading.

--- Modules/Loadable/Volumes/qSlicerVolumesReader.cxx
+++ Modules/Loadable/Volumes/qSlicerVolumesReader.cxx
@@ -150,6 +150,11 @@ bool qSlicerVolumesReader::load(const IOProperties& properties)
     {
     propagateVolumeSelection = properties["show"].toBool();
     }
+  // 0: do not fit slices
+  // 1: fit slice to all
+  // 2: fit slice to loaded volume
+  int fit = properties.value("fitSlicesToVolumes", 1).toInt();
+
   vtkSmartPointer<vtkStringArray> fileList;
   if (properties.contains("fileNames"))
     {
@@ -194,6 +199,13 @@ bool qSlicerVolumesReader::load(const IOProperties& properties)
         if (appLogic)
           {
           appLogic->PropagateVolumeSelection(); // includes FitSliceToAll by default
+        if (fit == 1)
+          {
+          appLogic->PropagateVolumeSelection(); // includes FitSliceToAll by default
+          }
+        else if (fit == 2)
+          {
+          appLogic->FitSlicesToVolume(node->GetID());
           }
         }
       }
